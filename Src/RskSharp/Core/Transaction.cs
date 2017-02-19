5c04f75a11d0a9b1bf9a602f8321b9b9c1469d2d
ajlopez <webmaster@ajlopez.com>
2017-02-19 11:44:54 -0300

    Initial empty classes in core

---
+++ Src/RskSharp/Core/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace RskSharp.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Transaction
+    {
+    }
+}
