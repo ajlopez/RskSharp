f0c50397f7f53c4627caa19337dddefe27f4debd
ajlopez <webmaster@ajlopez.com>
2017-02-20 15:33:57 -0300

    BlockHeaderBuilder first tests

---
+++ Src/RskSharp.Tests/Core/Builders/BlockHeaderBuilderTests.cs
@@ -0,0 +1,33 @@
+﻿namespace RskSharp.Tests.Core.Builders
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RskSharp.Core.Builders;
+    using RskSharp.Core;
+
+    [TestClass]
+    public class BlockHeaderBuilderTests
+    {
+        [TestMethod]
+        public void GetBlockZero()
+        {
+            BlockHeaderBuilder builder = new BlockHeaderBuilder();
+
+            BlockHeader block = builder.Build();
+
+            Assert.IsNotNull(block);
+            Assert.AreEqual(0, block.Number);
+        }
+
+        [TestMethod]
+        public void GetBlockWithNumber()
+        {
+            BlockHeaderBuilder builder = new BlockHeaderBuilder();
+
+            BlockHeader block = builder.Number(42).Build();
+
+            Assert.IsNotNull(block);
+            Assert.AreEqual(42, block.Number);
+        }
+    }
+}
---
+++ Src/RskSharp/Core/Builders/BlockHeaderBuilder.cs
@@ -0,0 +1,25 @@
+﻿namespace RskSharp.Core.Builders
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class BlockHeaderBuilder
+    {
+        private long number;
+
+        public BlockHeaderBuilder Number(long number)
+        {
+            this.number = number;
+
+            return this;
+        }
+
+        public BlockHeader Build()
+        {
+            return new BlockHeader(this.number);
+        }
+    }
+}
+
