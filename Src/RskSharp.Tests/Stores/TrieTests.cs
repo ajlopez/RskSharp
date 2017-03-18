0cac43449fb36ca36d10671fed4998d4d66d6c44
ajlopez <webmaster@ajlopez.com>
2017-03-18 15:03:12 -0300

    Trie first simple test

---
+++ Src/RskSharp.Tests/Stores/TrieTests.cs
@@ -0,0 +1,18 @@
+﻿namespace RskSharp.Tests.Stores
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using RskSharp.Stores;
+
+    [TestClass]
+    public class TrieTests
+    {
+        [TestMethod]
+        public void GetUnknownKeyValue()
+        {
+            Trie trie = new Trie();
+
+            Assert.IsNull(trie.GetValue(new byte[] { 0x01, 0x02, 0x03 }));
+        }
+    }
+}
---
+++ Src/RskSharp/Stores/Trie.cs
@@ -0,0 +1,15 @@
+﻿namespace RskSharp.Stores
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Trie
+    {
+        public byte[] GetValue(byte[] key)
+        {
+            return null;
+        }
+    }
+}
