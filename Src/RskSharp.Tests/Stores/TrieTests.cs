10bdb96d44cec6a7c8f36e223e3f31ab078ee674
ajlopez <webmaster@ajlopez.com>
2017-04-14 13:06:14 -0300

    Set Get Two Key Values in Trie, WIP

--- Src/RskSharp.Tests/Stores/TrieTests.cs
+++ Src/RskSharp.Tests/Stores/TrieTests.cs
@@ -28,5 +28,20 @@
             Assert.IsNotNull(result);
             Assert.AreEqual(value, result.GetValue(key));
         }
+
+        [TestMethod]
+        public void SetGetTwoKeyValues()
+        {
+            var key1 = new byte[] { 0x01, 0x02, 0x3 };
+            var value1 = new byte[] { 0x04, 0x05, 0x06 };
+            var key2 = new byte[] { 0x07, 0x08, 0x09 };
+            var value2 = new byte[] { 0x0a, 0x0b, 0x0c };
+
+            Trie result = new Trie().SetValue(key1, value1).SetValue(key2, value2);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(value1, result.GetValue(key1));
+            Assert.AreEqual(value2, result.GetValue(key2));
+        }
     }
 }
